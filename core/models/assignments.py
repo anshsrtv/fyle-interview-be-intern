6fdf3a8e7703b7ec56bca9173324c699e36143bf
anshsrtv <ansh_srtv@outlook.com>
2021-10-06 14:43:37 +0530

    feat: teacher APIs

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -60,6 +60,21 @@ class Assignment(db.Model):
         return assignment
 
     @classmethod
+    def grade_assignment(cls, _id, grade, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,
+                        'only a submitted assignment can be graded')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 
+                        'This assignment is submitted to some other teacher')
+
+        assignment.grade = grade
+        assignment.state = AssignmentStateEnum.GRADED
+        db.session.flush()
+
+        return assignment
+
+    @classmethod
     def submit(cls, _id, teacher_id, principal: Principal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
@@ -77,3 +92,7 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_student(cls, student_id):
         return cls.filter(cls.student_id == student_id).all()
+
+    @classmethod
+    def get_assignments_by_teacher(cls, teacher_id):
+        return cls.filter(cls.teacher_id == teacher_id).all()
